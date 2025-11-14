de5b793263fccf2ec5fcc06afccd897ee5be0e03
ualiurrahat <ualiurrahat25@gmail.com>
2025-11-14 21:39:15 +0600

    Add JS Day 04: operators and example codes

---
+++ 03 Javascript/Day 04/index.js
@@ -0,0 +1,208 @@
+// Operator
+// Arithmetic
+
+// console.log(2+5);
+// console.log(2-5);
+// console.log(2*5);
+// console.log(6/2);
+// console.log(5%2);
+// Reminder
+// console.log(5**3);
+
+// Assignment operator
+
+// let x = 20;
+// let y = 10;
+// x = x+y;
+// x = x-y;
+// x = x*y;
+// x+=y;
+// x = x+y;
+// x%=y;
+// console.log(x);
+
+// comparison operator
+// let x = 20;
+// let y= 10;
+
+// console.log(x>y);
+// console.log(x>=y);
+// console.log(x<y);
+// console.log(x<=y);
+// console.log(x==y);
+// console.log(x===y);
+
+// type check(dono same type ke hone chaiye ===, uske baad wo comparison)
+
+// number and string compare(string convert number)
+// String--> Number
+// let z = "120";
+// let a = Number(z);
+
+// console.log(a);
+// NaN: Not a number(Type of it is number)
+
+// let a = "121ac";
+// let b = Number(a);
+
+// console.log(typeof b);
+
+// let a = true;
+// console.log(Number(true));
+// console.log(Number(false));
+
+// console.log(Number(null));
+// console.log(Number(undefined));
+
+// null--> 0
+//  undefined-->NaN
+
+// Number -> String
+// let a = 10;
+// let b = String(a);
+
+// console.log(typeof b);
+// "true"
+// true
+
+// console.log(typeof String(undefined));
+
+// Boolean convert karunga
+
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+
+// computer science challenge
+
+// let a = 0.1;
+// let b = 0.2;
+
+// let c = a+b;
+
+// console.log(c);
+
+// 1: null is loosely equal to undefined only
+
+// console.log(null==undefined);
+// console.log(null===undefined);
+// console.log(null==0);
+// console.log(null=="");
+// console.log(null==false);
+// console.log(null==true);
+
+// >,<,>=,<= (null --> number, undefined --> NaN)
+
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=undefined);
+// console.log(undefined>=0);
+
+// console.log("Rohit">"Rahit");
+
+// console.log(10<true);
+
+// Ek koi bhi type hai, usko agar muje compare karna hai dusre kisi type se
+// Dono number mein convert honge
+
+// console.log(null>="");
+
+// console.log(NaN == NaN);
+
+// loop kaise kaam karte hai
+
+// for loop
+
+// post increment
+// post decrenment : i--
+
+// for(let i = 0; i<10;i++){
+//     console.log(i);
+// }
+
+// while loop
+
+// let i = 0;
+
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// do while
+
+// let i= 0;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<10);
+
+// if else condition
+
+// let age = 25;
+
+// if(age>=18){
+//     console.log("Eligible for vote");
+// }
+// else{
+//     console.log("Not eligible for vote");
+// }
+
+// kid young and old
+
+// let age = 78;
+
+// if(age<18){
+//     console.log("Kid");
+// }
+// else if(age>=60){
+//     console.log("Old");
+// }
+// else{
+//     console.log("young");
+// }
+
+// Logical Operator
+
+// logical And&& , ||
+
+// console.log(true&&true);
+// console.log(true&&false);
+// console.log(false&&false);
+// console.log(false&&true);
+
+// console.log(true||true);
+// console.log(true||false);
+// console.log(false||false);
+// console.log(false||true);
+
+// let a = "Rohit";
+// let b = "";
+// let c = a&&b;
+// console.log(c);
+
+// let a = 0;
+// let b = 20;
+
+// console.log(a&&b);
+// &&: If first value is false, it will return the first value itself
+// If first value is true, it will return second
+
+// || logical or
+
+let a = 0;
+let b = 20;
+
+console.log(a || b);
+// &&: If first value is true, it will return the first value itself
+// If first value is false, it will return second
+
+// !=
+// console.log(5!=5);
+
+// AND: & , | or
+
+console.log(2 & 5);
